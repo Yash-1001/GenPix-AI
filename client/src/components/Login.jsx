1f62fdc1925e82c6a0890daacb7aee82c8c96c64
Yash <ydwivedi007@gmail.com>
2025-05-30 23:02:21 +0530

    added some animations

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { assets } from '../assets/assets'
+import { AppContext } from '../context/AppContext';
 
 const Login = () => {
+  const [state,setState] = useState('Login'); 
+  const {setShowLogin} = useContext(AppContext); 
+
+
+  useEffect(()=>{
+    document.body.style.overflow = 'hidden';
+    return()=>{
+      document.body.style.overflow = 'unset';
+    }
+  },[])
   return (
     <div className='absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
       <form className='relative bg-white p-10 rounded-xl text-slate-500'>
-        <h1 className='text-center text-2xl text-neutral-700 font-medium'>Sign Up</h1>
+        <h1 className='text-center text-2xl text-neutral-700 font-medium'>{state}</h1>
         <p className='text-sm'>Welcome back! Please sign in to continue</p>
-        <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-5'>
+        {state !== 'Login' && <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-5'>
 
             <img  src={assets.lock_icon} alt="" />
             <input className='outlline-none text-sm' type="text" placeholder='Full Name' required/>
-        </div>
+        </div>}
         <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
-
             <img  src={assets.email_icon} alt="" />
             <input className='outlline-none text-sm' type="email" placeholder='Email id' required/>
         </div>
@@ -23,13 +33,14 @@ const Login = () => {
             <input className='outlline-none text-sm' type="password" placeholder='Password' required/>
         </div>
         <p className='text-sm text-blue-600 my-4 cursor-pointer'>Forgot Password?</p>
-        <button className='bg-blue-600 w-full text-white py-2 rounded-full'>Create Account</button>
-        <p className='mt-5 text-center'>Don't have an account? <span className='text-blue-600 cursor-pointer'> Sign Up</span>
+        <button className='bg-blue-600 w-full text-white py-2 rounded-full'>{state === 'Login' ? 'login' : 'create account'}</button>
+        {state === 'Login'? <p className='mt-5 text-center'>Don't have an account? <span className='text-blue-600 cursor-pointer' onClick={()=>setState('Sign Up')}> Sign Up</span>
         </p>
-        <p className='mt-5 text-center'> Already have an account? <span className='text-blue-600 cursor-pointer'> Login</span>
-        </p>
+        :
+        <p className='mt-5 text-center'> Already have an account? <span className='text-blue-600 cursor-pointer' onClick={()=>setState('Login')}> Login</span>
+        </p>}
 
-        <img src={assets.cross_icon} className='absolute top-5 right-5 cursor-pointer' alt="" />
+        <img src={assets.cross_icon} onClick={()=>setShowLogin(false)} className='absolute top-5 right-5 cursor-pointer'  alt="" />
       </form>
     </div>
   )
