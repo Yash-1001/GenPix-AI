2ff2c1a7a3d3de371119b47a74bd119cc0ec042c
Yash <ydwivedi007@gmail.com>
2025-06-07 10:00:59 +0530

    login functionality

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,12 +2,46 @@ import React, { useContext, useEffect, useState } from 'react'
 import { assets } from '../assets/assets'
 import { AppContext } from '../context/AppContext';
 import {motion} from 'framer-motion'
+import axios from 'axios'
+import { toast } from 'react-toastify';
 const Login = () => {
   const [state,setState] = useState('Login'); 
-  const {setShowLogin} = useContext(AppContext); 
+  const {setShowLogin,backendUrl,setToken,setUser } = useContext(AppContext); 
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      if(state==='Login'){
+       const {data} = await axios.post(backendUrl + '/api/user/login' ,{email,password} )
+       if (data.success){
+        setToken(data.token)
+        setUser(data.user)
+        localStorage.setItem('token', data.token)
+        setShowLogin(false)
+       }
+       else{
+        toast.error(data.message)
+       }
+      }else{
+          const {data} = await axios.post(backendUrl + '/api/user/register' ,{name, email,password} )
+       if (data.success){
+        setToken(data.token)
+        setUser(data.user)
+        localStorage.setItem('token', data.token)
+        setShowLogin(false)
+       }
+       else{
+        toast.error(data.message)
+       }
+      }
+    } catch (error) {
+       toast.error(error.message)
+
+    }
+  }
 
 
   useEffect(()=>{
@@ -18,7 +52,7 @@ const Login = () => {
   },[])
   return (
     <div className='fixed top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
-      <motion.form 
+      <motion.form onSubmit={onSubmitHandler}
        initial={{opacity:0.2, y:50}}
      transition={{duration:0.4}}
      whileInView={{opacity:1, y:0}}
@@ -29,16 +63,16 @@ const Login = () => {
         {state !== 'Login' && <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-5'>
 
             <img  src={assets.lock_icon} alt="" />
-            <input className='outlline-none text-sm' type="text" placeholder='Full Name' required/>
+            <input onChange={e=>setName(e.target.value)} value={name} className='outlline-none text-sm' type="text" placeholder='Full Name' required/>
         </div>}
         <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
             <img  src={assets.email_icon} alt="" />
-            <input className='outlline-none text-sm' type="email" placeholder='Email id' required/>
+            <input onChange={e=>setEmail(e.target.value)} value={email} className='outlline-none text-sm' type="email" placeholder='Email id' required/>
         </div>
         <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
 
             <img  src={assets.lock_icon} alt="" />
-            <input className='outlline-none text-sm' type="password" placeholder='Password' required/>
+            <input onChange={e=>setPassword(e.target.value)} value={password} className='outlline-none text-sm' type="password" placeholder='Password' required/>
         </div>
         <p className='text-sm text-blue-600 my-4 cursor-pointer'>Forgot Password?</p>
         <button className='bg-blue-600 w-full text-white py-2 rounded-full'>{state === 'Login' ? 'login' : 'create account'}</button>
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,4 +1,7 @@
 import { createContext, useState } from "react";
+import { toast } from "react-toastify";
+import axios from 'axios'
+import { useEffect } from "react";
 
 export const AppContext = createContext()
 const AppContextProvider =(props) => {
@@ -10,8 +13,34 @@ const AppContextProvider =(props) => {
 
 
     const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+    const loadCreditsData = async () =>{
+        try {
+            const{data} = await axios.get(backendUrl + '/api/user/credits' , {headers: {token}})
+
+            if(data.success){
+                setCredit(data.credits)
+                setUser(data.user)
+            }
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+            
+        }
+    }
+    const logout =()=> {
+        localStorage.removeItem('token')
+        setToken('')
+        setUser(null)
+    }
+    useEffect (()=>{
+        if(token){
+            loadCreditsData()
+        }
+
+    },[token])
     const value ={
-        user,setUser, showLogin, setShowLogin , backendUrl, token , setToken, credit, setCredit
+        user,setUser, showLogin, setShowLogin , backendUrl, token , setToken, credit, setCredit ,loadCreditsData, logout
     }
     return(
         <AppContext.Provider value={value}>
