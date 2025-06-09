4b8294c413dae86239d172ca7790970866f490e4
Yash <ydwivedi007@gmail.com>
2025-06-09 13:41:03 +0530

    server.js

--- server/server.js
+++ server/server.js
@@ -1,17 +1,35 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import path from 'path'
+import { fileURLToPath } from 'url'
 import connectDB from './config/mongodb.js'
 import userRouter from './routes/userRoutes.js'
 import imageRouter from './routes/imageRoutes.js'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 const PORT = process.env.PORT || 4000
 const app = express()
-app.use(express.json()) 
-app.use(cors()) 
+
+app.use(express.json())
+app.use(cors())
+
 await connectDB()
+
 app.use('/api/user', userRouter)
 app.use('/api/image', imageRouter)
-app.get('/',(req,res)=> res.send("API working"))
 
-app.listen(PORT,()=> console.log('server running on port ' + PORT));
+// API test route
+app.get('/', (req, res) => res.send("API working"))
+
+// Serve React frontend static files
+app.use(express.static(path.join(__dirname, '../client/build')))
+
+// For any other route, serve React's index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
+})
+
+app.listen(PORT, () => console.log('server running on port ' + PORT))
